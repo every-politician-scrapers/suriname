4df7fd1f1d0f2d91769ddfe9dabd6cd1f0ee7dff
Tony Bowden <tony@tmtm.com>
2022-09-13 19:06:25 +0300

    compare Foreign Ministers

---
+++ sources/wpoffice/foreign-minister/scraper.rb
@@ -0,0 +1,29 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no name img dates].freeze
+    end
+
+    def raw_combo_date
+      years = super.tidy
+      years =~ /^\d{4}$/ ? "#{years} - #{years}" : years
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
